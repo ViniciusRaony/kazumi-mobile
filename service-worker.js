91ad58d113490bf8d922812cfc1f75716bcc9123
Vinicius Raony <viniciusraony@hotmail.com>
2023-03-18 22:50:22 -0300

    Change png to webp

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'pwaTeste+-v1.2';
+var cacheName = 'pwaKazumi+-v1.2';
 
 self.addEventListener('install', event => {
 
@@ -11,14 +11,47 @@ self.addEventListener('install', event => {
         './index.html',
 
         './assets/img/favicon.png',
-        './assets/img/icon_128.png',
-        './assets/img/icon_144.png',
-        './assets/img/icon_152.png',
-        './assets/img/icon_167.png',
+        './assets/img/icon_50.png',
+        './assets/img/icon_57.png',
+        './assets/img/icon_60.png',
+        './assets/img/icon_80.png',
+        './assets/img/icon_87.png',
+        './assets/img/icon_114.png',
+        './assets/img/icon_120.png',
         './assets/img/icon_180.png',
-        './assets/img/icon_196.png',
-        './assets/img/icon_256.png',
-        './assets/img/icon_512.png',
+
+        './assets/img/about.webp',
+        './assets/img/about-2.webp',
+        './assets/img/destaque1.webp',
+        './assets/img/destaque2.webp',
+        './assets/img/destaque3.webp',
+        './assets/img/events1.webp',
+        './assets/img/events2.webp',
+        './assets/img/events3.webp',
+        './assets/img/reservation.webp',
+
+
+        './assets/img/gallery/gallery-1.webp',
+        './assets/img/gallery/gallery-2.webp',
+        './assets/img/gallery/gallery-3.webp',
+        './assets/img/gallery/gallery-4.webp',
+        './assets/img/gallery/gallery-5.webp',
+        './assets/img/gallery/gallery-6.webp',
+        './assets/img/gallery/gallery-7.webp',
+        './assets/img/gallery/gallery-8.webp',
+
+        './assets/img/menu/menu-item-1.webp',
+        './assets/img/menu/menu-item-2.webp',
+        './assets/img/menu/menu-item-3.webp',
+        './assets/img/menu/menu-item-4.webp',
+        './assets/img/menu/menu-item-5.webp',
+        './assets/img/menu/menu-item-6.webp',
+
+        './assets/img/testimonials/testimonials-1.webp',
+        './assets/img/testimonials/testimonials-2.webp',
+        './assets/img/testimonials/testimonials-3.webp',
+        './assets/img/testimonials/testimonials-4.webp',
+        
        
       ]))
   );
