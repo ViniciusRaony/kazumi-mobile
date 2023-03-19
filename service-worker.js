9c324bd02d9beebdf3b35ff0f9688af528558056
Vinicius Raony <viniciusraony@hotmail.com>
2023-03-19 00:31:40 -0300

    Fix youtube

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,8 @@ self.addEventListener('install', event => {
         './index.html',
         './assets/css/main.css',
         './assets/js/main.js',
-
         './forms/book-a-table.php',
         './forms/contact.php',
-
         './assets/img/favicon.png',
         './assets/img/icon_40.png',
         './assets/img/icon_57.png',
@@ -25,7 +23,6 @@ self.addEventListener('install', event => {
         './assets/img/icon_120.png',
         './assets/img/icon_180.png',
         './assets/img/icon_512.png',
-
         './assets/img/about.webp',
         './assets/img/about-2.webp',
         './assets/img/destaque1.webp',
@@ -36,7 +33,6 @@ self.addEventListener('install', event => {
         './assets/img/events-3.webp',
         './assets/img/reservation.webp',
         './assets/img/stats-bg.webp',
-
         './assets/img/gallery/gallery-1.webp',
         './assets/img/gallery/gallery-2.webp',
         './assets/img/gallery/gallery-3.webp',
@@ -45,14 +41,12 @@ self.addEventListener('install', event => {
         './assets/img/gallery/gallery-6.webp',
         './assets/img/gallery/gallery-7.webp',
         './assets/img/gallery/gallery-8.webp',
-
         './assets/img/menu/menu-item-1.webp',
         './assets/img/menu/menu-item-2.webp',
         './assets/img/menu/menu-item-3.webp',
         './assets/img/menu/menu-item-4.webp',
         './assets/img/menu/menu-item-5.webp',
         './assets/img/menu/menu-item-6.webp',
-
         './assets/img/testimonials/testimonials-1.webp',
         './assets/img/testimonials/testimonials-2.webp',
         './assets/img/testimonials/testimonials-3.webp',
@@ -69,24 +63,21 @@ self.addEventListener('message', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  //Atualizacao internet
-  event.respondWith(async function () {
-     try {
-       return await fetch(event.request);
-     } catch (err) {
-       return caches.match(event.request);
-     }
-   }());
+    // Verifica se a requisição é um vídeo do YouTube
+    if (event.request.url.includes('youtube.com') || event.request.url.includes('googlevideo.com')) {
+        return;
+    }
 
-  //Atualizacao cache
-  /*event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
+    // Tenta buscar a requisição no cache
+    event.respondWith(async function () {
+        try {
+            const cacheResponse = await caches.match(event.request);
+            if (cacheResponse) {
+                return cacheResponse;
+            }
+            return fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request);
         }
-        return fetch(event.request);
-      })
-  );*/
-
+    }());
 });
